94944efb2217317ef55268ee5a402c6d727a2158
Arpan Patel <ap.patel1893@gmail.com>
2019-04-26 00:17:12 -0500

    format person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,19 @@
 /* eslint-disable react/no-typos */
 import React, { Component, Fragment } from "react";
-import withClass from "../../../hoc/withClass"; 
-import classes from './Person.css';
-import PropTypes from 'prop-types';
-import AuthContext from '../../../context/auth-context'
+import withClass from "../../../hoc/withClass";
+import classes from "./Person.css";
+import PropTypes from "prop-types";
+import AuthContext from "../../../context/auth-context";
 
 class Person extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.inutElementRef = React.createRef();
   }
-  
+
   static contextType = AuthContext;
 
-  componentDidMount(){
+  componentDidMount() {
     // this.inutElement.focus();
     this.inutElementRef.current.focus();
     console.log(this.context.authenticated);
@@ -23,9 +23,12 @@ class Person extends Component {
     console.log("person js remder");
     return (
       <Fragment>
-        
-        {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-    
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
+
         <p onClick={this.props.click}>
           {this.props.name} is {this.props.age} years old
         </p>
@@ -41,7 +44,6 @@ class Person extends Component {
     );
   }
 }
-
 
 Person.propTypes = {
   click: PropTypes.func,
